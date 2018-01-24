3d160f54015e2da28eacff2e602c74c87e6c1bc2
Jose Hinojo <burningremedy@gmail.com>
2018-01-24 00:13:56 -0600

    Finish Exercise 6 Section 3

--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -69,19 +74,25 @@ public class MainActivity extends AppCompatActivity {
         mNumbersList.setAdapter(mAdapter);
     }
 
-    // TODO (2) Create a menu resource in res/menu/ called main.xml
-    // TODO (3) Add one item to the menu with an ID of action_refresh
-    // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
-    // TODO (5) Set the orderInCategory value to 1 to make sure this item is the first in the list
-    // TODO (6) Set app:showAsAction to ifRoom to display the menu item in the ActionBar if there is room
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
 
-    // TODO (7) Override onCreateOptionsMenu
-    // TODO (8) Use getMenuInflater().inflate to inflate the menu
-    // TODO (9) Return true to display this menu
-
-    // TODO (10) Override onOptionsItemSelected
-    // TODO (11) Within this method, get the ID from the MenuItem
-    // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
-    // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected
+        switch(item.getItemId()){
+            case R.id.action_refresh:
+                Toast toast = Toast.makeText(this,"Refreshed...",Toast.LENGTH_SHORT);
+                toast.show();
+                mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+                mNumbersList.setAdapter(mAdapter);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
