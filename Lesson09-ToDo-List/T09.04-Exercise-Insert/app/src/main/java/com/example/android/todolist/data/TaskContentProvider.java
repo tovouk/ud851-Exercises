68d69f104ffc5962087e57f0df14a83a30ad9509
Jose Hinojo <burningremedy@gmail.com>
2018-03-18 04:36:00 -0500

    Finish Ex.4 Section 9

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -17,12 +17,16 @@
 package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+
+import static com.example.android.todolist.data.TaskContract.TaskEntry.TABLE_NAME;
 
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
@@ -78,16 +82,31 @@ public class TaskContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues values) {
-        // TODO (1) Get access to the task database (to write new data to)
 
-        // TODO (2) Write URI matching code to identify the match for the tasks directory
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
 
-        // TODO (3) Insert new values into the database
-        // TODO (4) Set the value for the returnedUri and write the default case for unknown URI's
+        int match = sUriMatcher.match(uri);
 
-        // TODO (5) Notify the resolver if the uri has been changed, and return the newly inserted URI
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        Uri returnUri;
+
+        switch(match){
+            case TASKS:
+                long id = db.insert(TABLE_NAME,null,values);
+                if(id>0){
+                    returnUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI,id);
+                }else{
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown Uri: " + uri);
+        }
+
+
+        getContext().getContentResolver().notifyChange(uri,null);
+
+        return returnUri;
     }
 
 
